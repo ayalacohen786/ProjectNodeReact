1166281b3d4f9bad7aaa573542991f48dc109134
ayalacohen786 <ac33116878@gmail.com>
2025-03-18 17:41:04 +0200

    initial backend and fronted

---
+++ Backend/models/Projects.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const projectSchema = new mongoose.Schema({
+   
+});
+
+module.exports = mongoose.model('Projects', projectSchema);
---
+++ Backend/models/Tasks.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const tasksSchema = new mongoose.Schema({
+    
+});
+
+module.exports = mongoose.model('Tasks', tasksSchema);
+
